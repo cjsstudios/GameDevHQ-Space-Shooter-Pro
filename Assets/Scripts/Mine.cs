756abb9abb0d5950fa4de6a68bc83b8e20d45487
cjsstudios <60586529+cjsstudios@users.noreply.github.com>
2020-02-18 21:11:58 -0500

    Space Mine Setup

--- Assets/Scripts/Mine.cs
+++ Assets/Scripts/Mine.cs
@@ -8,6 +8,7 @@ public class Mine : MonoBehaviour
     private SoundFX _explosionSoundFX;  //<SoundFX> ref
     private static int _minesInSpace;   //Count of mines that drifted off screen
     public Vector3 mineLaunchDir;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,17 +27,14 @@ public class Mine : MonoBehaviour
                 break;
             default: mineLaunchDir = Vector3.up; Debug.Log("Default mineLaunchDir Chosen");
                 break;
-        }
-
-
-        
+        }        
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(mineLaunchDir * 3 * Time.deltaTime);
-        if (transform.position.y > 8.0f)                                //If laser off screen-top, destroy parent, then self
+        if (transform.position.y > 8.0f)                                //If mine off screen-top, destroy parent, then self
         {
             //if object has parent
             //destroy parent
@@ -48,29 +46,11 @@ public class Mine : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-    /*
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Enemy")
-        {
-            Debug.Log("Mine hit Enemy!!!");
-            Invoke("DestroyMineFire", 0.25f);
-        }
-    }
-    */
-    private void GetRandomSpawnDir()
-    {
-
-    }
+ 
     private void OnDestroy()
     {
         _explosionSoundFX.PlayExplosionSFX();           //Play soundFx
         Destroy(GetComponent<CircleCollider2D>());      //Destroy collider on mine
         Destroy(this.gameObject, 2.8f);                 //Destroy self  
-    }
-
-    void DestroyMineFire()
-    {
-
     }
 }
